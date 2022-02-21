cd0dfe6dbf397f884ddcaa783a8e275e357f1da4
Parmesan-Cheez <victoryan001@gmail.com>
2022-02-21 11:57:55 -0700

    fix

--- src/main/java/frc/parent/RobotMap.java
+++ src/main/java/frc/parent/RobotMap.java
@@ -10,15 +10,12 @@ package frc.parent;
     This ranges from talon ports, all the way to the ports
     on the controllers. This also contains the polarity for the wheels
     and the various ports assoiated with sensors
-
     If you wish to create your own port, here is the syntax:
         public static final returnType name = value;
     Notes on creating ports:
         1. Ports must be integers or booleans
         2. they MUST be public static final;
         3. If the port is not plugged in, make int values -1, and boolean values false
-
-
 */
 public interface RobotMap {
 
