955e88415e3212e00fac5410db4155bc801e40c2
The Victor Haha <victoryan001@gmail.com>
2022-03-03 16:23:09 -0700

    remap

--- src/main/java/frc/parent/RobotMap.java
+++ src/main/java/frc/parent/RobotMap.java
@@ -27,8 +27,8 @@ public interface RobotMap {
     public static final int CLIMBER = 4;
     public static final int SHOOTER = 1;
     public static final int SHOOTER2 = 7;
-    public static final int LOADER = 11;
-    public static final int SUCKY = 10;
+    public static final int LOADER = 8;
+    public static final int SUCKY = 9;
 
 
     //Wheel Polarities
